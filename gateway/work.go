477a3c9d9325eacbf70076575b712466869b706f
Alexander Sadovskyi <alexandersadovskyi7@protonmail.com>
2020-08-03 21:31:30 +0200

    Debug print

--- gateway/work.go
+++ gateway/work.go
@@ -18,6 +18,7 @@ package gateway
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -163,6 +164,7 @@ func (g *Gateway) validateShare(submittedWork []string, workerName string) (type
 	shareIsValid, actualTarget := hasher.Verify(share)
 
 	if shareIsValid {
+		fmt.Println("debug", fullWork[2], utils.HexStrToBigInt(fullWork[2]))
 		if utils.HexStrToBigInt(fullWork[2]).Cmp(actualTarget) > 0 {
 			go g.submitBlock(submittedWork, blockNumber, workerName, actualTarget)
 			g.parentWorkManager.BestShareTarget = utils.BigMax256bit
